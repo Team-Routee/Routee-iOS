af3f601af134534d68b637bce5b123e68e7dfc04
sangyup <sangyeobi712@gmail.com>
2026-06-25 11:15:22 +0900

    [setting] - 초기 프로젝트 세팅

---
+++ Routee-iOS/Routee-iOS/Presentation/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Routee-iOS
+//
+//  Created by LEESANGYUP on 6/22/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
