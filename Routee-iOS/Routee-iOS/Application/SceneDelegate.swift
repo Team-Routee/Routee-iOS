1efe88e605b2e330ffca70ecb7fd193d1d44e8cd
sangyup <sangyeobi712@gmail.com>
2026-06-27 09:26:26 +0900

    [style] #3 - 샘플뷰

--- Routee-iOS/Routee-iOS/Application/SceneDelegate.swift
+++ Routee-iOS/Routee-iOS/Application/SceneDelegate.swift
@@ -16,8 +16,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
+        let viewController = SampleViewController()
+        let navigationViewController = UINavigationController(rootViewController: viewController)
+        
+        window.rootViewController = navigationViewController
         self.window = window
-        window.rootViewController = UINavigationController(rootViewController: ViewController())
         window.makeKeyAndVisible()
     }
 
