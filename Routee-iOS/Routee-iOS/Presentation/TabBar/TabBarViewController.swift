a15a4a9b91c45935cc58ff398f6d7ee8dd82860c
seunghee <daalgwabyul@naver.com>
2026-07-06 15:35:29 +0900

    [chore] #12 - radius 값 변경 및 변수 이름 수정

--- Routee-iOS/Routee-iOS/Presentation/TabBar/TabBarViewController.swift
+++ Routee-iOS/Routee-iOS/Presentation/TabBar/TabBarViewController.swift
@@ -11,202 +11,198 @@ import SnapKit
 import Then
 
 final class TabBarViewController: UITabBarController {
-
+    
     // MARK: - Properties
-
-    private let exerciseItem = TabBarItem()
-    private let recapItem = TabBarItem()
+    
+    private let workoutItem = TabBarItem()
+    private let recordEditItem = TabBarItem()
     private let archiveItem = TabBarItem()
     private let settingItem = TabBarItem()
-
+    
     private lazy var tabBarItems = [
-        exerciseItem,
-        recapItem,
+        workoutItem,
+        recordEditItem,
         archiveItem,
         settingItem
     ]
-
+    
     private var didSetInitialSelection = false
-
+    
     // MARK: - UI Properties
-
+    
     private let customTabBar = UIView()
     private let selectionView = UIView()
-
+    
     // MARK: - Life Cycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         configureViewControllers()
         configureTabItems()
-
+        
         setStyle()
         setUI()
         setLayout()
-
+        
         tabBar.isHidden = true
     }
-
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-
+        
         guard !didSetInitialSelection else { return }
         didSetInitialSelection = true
-
+        
         updateSelection(index: 0, animated: false)
     }
-}
-
-// MARK: - Configure
-
-private extension TabBarViewController {
-
-    func configureViewControllers() {
-        let exercise = UINavigationController(rootViewController: SampleViewController())
-        let recap = UINavigationController(rootViewController: SampleViewController())
+    
+    // MARK: - Configure
+    
+    private func configureViewControllers() {
+        let workout = UINavigationController(rootViewController: SampleViewController())
+        let recordEdit = UINavigationController(rootViewController: SampleViewController())
         let archive = UINavigationController(rootViewController: SampleViewController())
         let setting = UINavigationController(rootViewController: SampleViewController())
-
-        exercise.navigationBar.isHidden = true
-        recap.navigationBar.isHidden = true
-        archive.navigationBar.isHidden = true
-        setting.navigationBar.isHidden = true
-
+        
+        [workout, recordEdit, archive, setting].forEach { $0.navigationBar.isHidden = true }
+        
         viewControllers = [
-            exercise,
-            recap,
+            workout,
+            recordEdit,
             archive,
             setting
         ]
     }
-
-    func configureTabItems() {
-        exerciseItem.configure(
+    
+    private func configureTabItems() {
+        workoutItem.configure(
             normalImage: .icExerciseNavSmGrey,
             selectedImage: .icExerciseNavSmWhite,
             title: "운동"
         )
-
-        recapItem.configure(
+        
+        recordEditItem.configure(
             normalImage: .icRecapNavSmGrey,
             selectedImage: .icRecapNavSmWhite,
             title: "기록 편집"
         )
-
+        
         archiveItem.configure(
             normalImage: .icArchiveNavSmGrey,
             selectedImage: .icArchiveNavSmWhite,
             title: "아카이브"
         )
-
+        
         settingItem.configure(
             normalImage: .icSettingNavSmGrey,
             selectedImage: .icSettingNavSmWhite,
             title: "설정"
         )
-
-        for (index, item) in tabBarItems.enumerated() {
+        
+        tabBarItems.enumerated().forEach { index, item in
             item.tag = index
-
-            item.addTarget(self, action: #selector(tabDidTap(_:)), for: .touchUpInside
+            
+            item.addTarget(
+                self,
+                action: #selector(tabDidTap(_:)),
+                for: .touchUpInside
             )
         }
     }
-
-    func setStyle() {
+    
+    // MARK: - UI Setting
+    
+    private func setStyle() {
         customTabBar.do {
             $0.backgroundColor = .grey900
-            $0.layer.cornerRadius = 32
+            $0.layer.cornerRadius = .r32
             $0.layer.masksToBounds = true
         }
-
+        
         selectionView.do {
             $0.backgroundColor = .white10
-            $0.layer.cornerRadius = 27
+            $0.layer.cornerRadius = 25
             $0.layer.masksToBounds = true
         }
     }
-
-    func setUI() {
+    
+    private func setUI() {
         view.addSubview(customTabBar)
-
+        
         customTabBar.addSubviews(
             selectionView,
-            exerciseItem,
-            recapItem,
+            workoutItem,
+            recordEditItem,
             archiveItem,
             settingItem
         )
     }
-
-    func setLayout() {
+    
+    private func setLayout() {
         customTabBar.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.bottom.equalTo(view.safeAreaLayoutGuide)
             $0.width.equalTo(343)
             $0.height.equalTo(62)
         }
-
+        
         selectionView.snp.makeConstraints {
-            $0.leading.equalToSuperview().offset(6)
+            $0.top.leading.equalToSuperview().inset(6)
+            $0.width.equalTo(79)
+            $0.height.equalTo(50)
+        }
+        
+        workoutItem.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(6)
             $0.top.bottom.equalToSuperview().inset(6)
             $0.width.equalTo(79)
         }
-
-        exerciseItem.snp.makeConstraints {
-            $0.leading.equalToSuperview().offset(6)
-            $0.top.equalToSuperview().offset(6)
-            $0.bottom.equalToSuperview().inset(6)
+        
+        recordEditItem.snp.makeConstraints {
+            $0.leading.equalTo(workoutItem.snp.trailing).offset(5)
+            $0.top.bottom.equalToSuperview().inset(6)
             $0.width.equalTo(79)
         }
-
-        recapItem.snp.makeConstraints {
-            $0.leading.equalTo(exerciseItem.snp.trailing).offset(5)
-            $0.top.equalToSuperview().offset(6)
-            $0.bottom.equalToSuperview().inset(6)
-            $0.width.equalTo(79)
-        }
-
+        
         archiveItem.snp.makeConstraints {
-            $0.leading.equalTo(recapItem.snp.trailing).offset(5)
-            $0.top.equalToSuperview().offset(6)
-            $0.bottom.equalToSuperview().inset(6)
+            $0.leading.equalTo(recordEditItem.snp.trailing).offset(5)
+            $0.top.bottom.equalToSuperview().inset(6)
             $0.width.equalTo(79)
         }
-
+        
         settingItem.snp.makeConstraints {
             $0.leading.equalTo(archiveItem.snp.trailing).offset(5)
-            $0.top.equalToSuperview().offset(6)
-            $0.bottom.equalToSuperview().inset(6)
+            $0.top.bottom.equalToSuperview().inset(6)
             $0.width.equalTo(79)
         }
     }
-    func updateSelection(index: Int, animated: Bool = true) {
+    
+    // MARK: - Private Methods
+    
+    private func updateSelection(index: Int, animated: Bool = true) {
         selectedIndex = index
         updateItemAppearance(selectedIndex: index)
         moveSelectionView(to: index, animated: animated)
     }
-
-    func updateItemAppearance(selectedIndex: Int) {
+    
+    private func updateItemAppearance(selectedIndex: Int) {
         tabBarItems.enumerated().forEach { index, item in
             item.isSelected = index == selectedIndex
         }
     }
-
-    func moveSelectionView(to index: Int, animated: Bool) {
+    
+    private func moveSelectionView(to index: Int,animated: Bool) {
         let targetItem = tabBarItems[index]
-
+        
         selectionView.snp.remakeConstraints {
             $0.width.equalTo(79)
             $0.top.bottom.equalToSuperview().inset(6)
             $0.centerX.equalTo(targetItem)
         }
-
-        let animation = {
-            self.customTabBar.layoutIfNeeded()
-        }
-
+        
+        let animation = { self.customTabBar.layoutIfNeeded() }
+        
         if animated {
             UIView.animate(withDuration: 0.1) {
                 animation()
@@ -215,14 +211,11 @@ private extension TabBarViewController {
             animation()
         }
     }
-}
-
-// MARK: - Action
-
-private extension TabBarViewController {
-
+    
+    // MARK: - Actions
+    
     @objc
-    func tabDidTap(_ sender: TabBarItem) {
+    private func tabDidTap(_ sender: TabBarItem) {
         updateSelection(index: sender.tag)
     }
 }
