9e2d5535b12236ac73afb39884e3cd8d12389d5b
sangyup <sangyeobi712@gmail.com>
2026-07-06 21:56:02 +0900

    [chore] #2 - NicknameSettingViewController 마크 주석

--- Routee-iOS/Routee-iOS/Presentation/Onboarding/ViewController/NicknameSettingViewController.swift
+++ Routee-iOS/Routee-iOS/Presentation/Onboarding/ViewController/NicknameSettingViewController.swift
@@ -10,18 +10,24 @@ import UIKit
 final class NicknameSettingViewController: BaseUIViewController {
     let nicknameSettingView = NicknameSettingView()
     
+    // MARK: - Life Cycle
+    
     override func loadView() {
         view = nicknameSettingView
     }
     
+    // MARK: - Add Targets
+    
     override func setAddTarget() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapEmptyArea))
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapKeyboardHide))
         tapGesture.cancelsTouchesInView = false
         view.addGestureRecognizer(tapGesture)
     }
-    
+        
+    // MARK: - Actions
+
     @objc
-    private func didTapEmptyArea() {
+    private func didTapKeyboardHide() {
         view.endEditing(true)
     }
 }
