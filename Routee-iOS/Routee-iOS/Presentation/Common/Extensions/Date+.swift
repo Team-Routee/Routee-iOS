64e4e3099d0882d1560f1f74dc6465cd9a6ffc2c
ser0kim <srkim0728@gmail.com>
2026-07-07 01:14:59 +0900

    [feat] #13 - Date+ extansion 구현

---
+++ Routee-iOS/Routee-iOS/Presentation/Common/Extensions/Date+.swift
@@ -0,0 +1,22 @@
+//
+//  Date+.swift
+//  Routee-iOS
+//
+//  Created by 김세령 on 7/7/26.
+//
+
+import UIKit
+
+extension Date {
+    var startOfMonth: Date {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month], from: self)
+        return calendar.date(from: components) ?? self
+    }
+    
+    func isSameMonth(as date: Date) -> Bool {
+        let calendar = Calendar.current
+        return calendar.component(.year, from: self) == calendar.component(.year, from: date)
+        && calendar.component(.month, from: self) == calendar.component(.month, from: date)
+    }
+}
