a28be25c9ca7de150fba9e012892fc656a9fb95d
seunghee <daalgwabyul@naver.com>
2026-07-06 13:42:28 +0900

    [style] #12 - 탭 바 아이템 stackView로 수정

--- Routee-iOS/Routee-iOS/Presentation/TabBar/TabBarItem.swift
+++ Routee-iOS/Routee-iOS/Presentation/TabBar/TabBarItem.swift
@@ -4,70 +4,81 @@
 //
 //  Created by 초긍정행운의포춘쿠키 on 7/5/26.
 //
-
 import UIKit
 
 import SnapKit
 import Then
 
 final class TabBarItem: UIControl {
-    
+
     // MARK: - Properties
-    
+
     private var normalImage: UIImage?
     private var selectedImage: UIImage?
-    
+
     override var isSelected: Bool {
-        didSet { updateAppearance() }
+        didSet {
+            updateAppearance()
+        }
     }
-    
+
     // MARK: - UI Properties
-    
+
     private let imageView = UIImageView()
     private let titleLabel = UILabel()
-    
+
+    private lazy var contentStackView = UIStackView()
+
     // MARK: - Initialize
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         setStyle()
         setUI()
         setLayout()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     // MARK: - Private
-    
+
     private func setStyle() {
         imageView.do {
             $0.contentMode = .scaleAspectFit
         }
-        
+
         titleLabel.do {
             $0.font = .label_m_12
             $0.textAlignment = .center
         }
-    }
-    
-    private func setUI() {
-        addSubviews(imageView, titleLabel)
-    }
-    
-    private func setLayout() {
-        imageView.snp.makeConstraints {
-            $0.top.equalToSuperview()
-            $0.centerX.equalToSuperview()
-            $0.width.height.equalTo(24)
+
+        contentStackView.do {
+            $0.axis = .vertical
+            $0.alignment = .center
+            $0.distribution = .fill
+            $0.spacing = 0
+            $0.isUserInteractionEnabled = false
         }
-        
+    }
+
+    private func setUI() {
+        addSubview(contentStackView)
+        contentStackView.addArrangedSubviews(imageView,titleLabel)
+    }
+
+    private func setLayout() {
+        contentStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+
+        imageView.snp.makeConstraints {
+            $0.size.equalTo(24)
+        }
+
         titleLabel.snp.makeConstraints {
-            $0.top.equalTo(imageView.snp.bottom)
-            $0.centerX.equalToSuperview()
-            $0.bottom.equalToSuperview()
             $0.height.equalTo(17)
         }
     }
