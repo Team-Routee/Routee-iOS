0d7fdc16debcdc06f55ff6dab486ce4ad7490085
ser0kim <srkim0728@gmail.com>
2026-07-06 20:28:01 +0900

    [style] #13 - RecordEditViewController 구현

--- Routee-iOS/Routee-iOS/Presentation/RecordEdit/Main/RecordEditViewController.swift
+++ Routee-iOS/Routee-iOS/Presentation/RecordEdit/Main/RecordEditViewController.swift
@@ -9,3 +9,59 @@ import UIKit
 
 import SnapKit
 import Then
+
+final class RecordEditViewController: BaseUIViewController, UICollectionViewDelegate {
+    
+    private let rootView = RecordEditView()
+    
+    private let records = [
+        "숭실대 동기모임 2탄",
+        "북한산 백운대 코스",
+        "아차산 야경 산책",
+        "인왕산 둘레길"
+    ]
+    
+    override func loadView() {
+        view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setCollectionView()
+    }
+    
+    private func setCollectionView() {
+        rootView.workoutRecordCollectionView.register(
+            WorkoutRecordCell.self,
+            forCellWithReuseIdentifier: WorkoutRecordCell.identifier
+        )
+        
+        rootView.workoutRecordCollectionView.dataSource = self
+        rootView.workoutRecordCollectionView.delegate = self
+    }
+}
+
+extension RecordEditViewController: UICollectionViewDataSource {
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return records.count
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: WorkoutRecordCell.identifier,
+            for: indexPath
+        ) as? WorkoutRecordCell else {
+            return UICollectionViewCell()
+        }
+        
+        return cell
+    }
+}
