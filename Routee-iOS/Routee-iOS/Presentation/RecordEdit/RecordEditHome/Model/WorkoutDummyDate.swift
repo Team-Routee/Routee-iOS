cd5cb5f7b5e92d7000cc41bdb51a99e4a9815999
ser0kim <srkim0728@gmail.com>
2026-07-07 12:07:59 +0900

    [feat] #13 - 더미 데이터 구현

---
+++ Routee-iOS/Routee-iOS/Presentation/RecordEdit/RecordEditHome/Model/WorkoutDummyDate.swift
@@ -0,0 +1,54 @@
+//
+//  WorkoutDummyDate.swift
+//  Routee-iOS
+//
+//  Created by 김세령 on 7/7/26.
+//
+
+import Foundation
+
+enum WorkoutDummyData {
+
+    static let dummyWorkoutRecords: [WorkoutRecordModel] = [
+
+        // MARK: - 7월
+        .init(title: "북한산 백운대 코스", date: .date(2026, 7, 20)),
+        .init(title: "올림픽공원 러닝", date: .date(2026, 7, 18)),
+        .init(title: "한강 저녁 러닝", date: .date(2026, 7, 15)),
+        .init(title: "아차산 일출 산책", date: .date(2026, 7, 12)),
+        .init(title: "관악산 연주대 등산", date: .date(2026, 7, 8)),
+        .init(title: "숭실대 동기모임 2탄", date: .date(2026, 7, 3)),
+
+        // MARK: - 6월
+        .init(title: "북한산 숨은벽 코스", date: .date(2026, 6, 29)),
+        .init(title: "서울숲 산책", date: .date(2026, 6, 25)),
+        .init(title: "한강 새벽 러닝", date: .date(2026, 6, 21)),
+        .init(title: "남산 둘레길", date: .date(2026, 6, 17)),
+        .init(title: "청계산 등산", date: .date(2026, 6, 10)),
+        .init(title: "아차산 야간 산책", date: .date(2026, 6, 4)),
+
+        // MARK: - 5월
+        .init(title: "한강 자전거 라이딩", date: .date(2026, 5, 28)),
+        .init(title: "북서울꿈의숲 걷기", date: .date(2026, 5, 24)),
+        .init(title: "관악산 둘레길", date: .date(2026, 5, 20)),
+        .init(title: "안산 메타세쿼이아길", date: .date(2026, 5, 16)),
+        .init(title: "석촌호수 러닝", date: .date(2026, 5, 9)),
+        .init(title: "여의도 한강공원", date: .date(2026, 5, 2)),
+
+        // MARK: - 4월
+        .init(title: "인왕산 둘레길", date: .date(2026, 4, 27)),
+        .init(title: "북악산 성곽길", date: .date(2026, 4, 22)),
+        .init(title: "서울숲 피크닉", date: .date(2026, 4, 18)),
+        .init(title: "불암산 야간 산행", date: .date(2026, 4, 13)),
+        .init(title: "양재천 벚꽃 산책", date: .date(2026, 4, 7)),
+        .init(title: "올림픽공원 러닝", date: .date(2026, 4, 1)),
+
+        // MARK: - 3월
+        .init(title: "숭실대 동기모임 2탄", date: .date(2026, 3, 23)),
+        .init(title: "북한산 백운대 코스", date: .date(2026, 3, 19)),
+        .init(title: "백석동천 저녁 러닝", date: .date(2026, 3, 14)),
+        .init(title: "숭실대 동기모임 북한산", date: .date(2026, 3, 12)),
+        .init(title: "북악산 둘레길", date: .date(2026, 3, 7)),
+        .init(title: "한강 새벽 러닝", date: .date(2026, 3, 2))
+    ]
+}
---
+++ Routee-iOS/Routee-iOS/Presentation/RecordEdit/RecordEditHome/Model/WorkoutRecordModel.swift
@@ -0,0 +1,13 @@
+//
+//  WorkoutRecordModel.swift
+//  Routee-iOS
+//
+//  Created by 김세령 on 7/7/26.
+//
+
+import Foundation
+
+struct WorkoutRecordModel {
+    let title: String
+    let date: Date
+}
