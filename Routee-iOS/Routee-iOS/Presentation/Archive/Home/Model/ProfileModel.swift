17901f2c1cda3fa572bd8a334f35888cb6b9196c
seunghee <daalgwabyul@naver.com>
2026-07-07 17:40:38 +0900

    [style] #17 - 프로필 정보 모델 생성

---
+++ Routee-iOS/Routee-iOS/Presentation/Archive/Home/Model/ProfileModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileModel.swift
+//  Routee-iOS
+//
+//  Created by 초긍정행운의포춘쿠키 on 7/7/26.
+//
+
+struct ProfileModel {
+    let userName: String
+    let streakNumber: Int
+    let pointNumber: Int
+    let profileImageName: String
+}
