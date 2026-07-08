ac34a4bafb49c39da6199d56c6fea2d7916fa3d8
seunghee <daalgwabyul@naver.com>
2026-07-08 18:09:45 +0900

    [style] #17 - 활동 목록 모달 구현

---
+++ Routee-iOS/Routee-iOS/Presentation/Archive/List/ViewController/ListViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ArchiveListViewController.swift
+//  Routee-iOS
+//
+//  Created by 초긍정행운의포춘쿠키 on 7/7/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ArchiveListViewController: BaseUIViewController {
+
+    private let rootView = ArchiveListView()
+    private let model: ListModel
+
+    init(model: ListModel = ArchiveListDummyData.defaultModel) {
+        self.model = model
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    convenience init(dateText: String, count: Int) {
+        self.init(model: ArchiveListDummyData.makeModel(dateText: dateText, count: count))
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func setView() {
+        view = rootView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        rootView.configure(with: model)
+        configureSheet()
+    }
+
+    private func configureSheet() {
+        preferredContentSize = CGSize(width: 0, height: ArchiveListView.modalHeight(for: model.items.count))
+
+        guard let sheet = sheetPresentationController else { return }
+
+        let targetHeight = ArchiveListView.modalHeight(for: model.items.count)
+        sheet.detents = [
+            .custom { _ in targetHeight }
+        ]
+        sheet.prefersGrabberVisible = false
+        sheet.preferredCornerRadius = 24
+        sheet.prefersScrollingExpandsWhenScrolledToEdge = false
+    }
+}
