05c514e5c781e8ce30a03efea64a2120adb4813b
ser0kim <srkim0728@gmail.com>
2026-07-10 01:41:43 +0900

    [style] #21 - 탭바 스티커 탭 구현

---
+++ Routee-iOS/Routee-iOS/Presentation/RecordEdit/Editor/View/EditorView.swift
@@ -0,0 +1,84 @@
+//
+//  EditorView.swift
+//  Routee-iOS
+//
+//  Created by 김세령 on 7/8/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class EditorView: BaseUIView {
+
+    // MARK: - UI Properties
+
+    let topNavigationBar = TopNavigationBar(rightTitle: "완료")
+    private let backgroundImageView = UIImageView()
+    private let dataInfo = RecordInfo()
+    private let recordEditTabBar = RecordEditTabBar()
+    private lazy var hideOptionViewTapGesture = UITapGestureRecognizer(
+        target: self,
+        action: #selector(handleViewTapped(_:))
+    )
+
+    // MARK: - UI Setting
+
+    override func setStyle() {
+        backgroundColor = .bgPrimary
+        hideOptionViewTapGesture.cancelsTouchesInView = false
+
+        backgroundImageView.do {
+            $0.image = UIImage(resource: .imgNavermapMain)
+        }
+    }
+
+    override func setUI() {
+        addSubviews(
+            backgroundImageView,
+            dataInfo,
+            topNavigationBar,
+            recordEditTabBar
+        )
+
+        addGestureRecognizer(hideOptionViewTapGesture)
+    }
+
+    override func setLayout() {
+        topNavigationBar.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
+        }
+
+        recordEditTabBar.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(safeAreaLayoutGuide)
+            $0.height.equalTo(71)
+        }
+
+        backgroundImageView.snp.makeConstraints {
+            $0.top.equalTo(topNavigationBar.snp.bottom).offset(12)
+            $0.bottom.equalTo(recordEditTabBar.snp.top)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+
+        dataInfo.snp.makeConstraints {
+            $0.top.equalTo(backgroundImageView.snp.top).offset(80)
+            $0.leading.equalTo(backgroundImageView.snp.leading).offset(32)
+            $0.height.equalTo(164)
+            $0.width.equalTo(85)
+        }
+    }
+
+    // MARK: - Actions
+
+    @objc
+    private func handleViewTapped(_ gesture: UITapGestureRecognizer) {
+        let point = gesture.location(in: recordEditTabBar)
+
+        guard !recordEditTabBar.containsInteractivePoint(point) else { return }
+
+        recordEditTabBar.hideOptionView()
+    }
+}
