619b12a0ede5be654433c35a386dd4eb7be9b1e7
sangyup <sangyeobi712@gmail.com>
2026-07-09 18:11:59 +0900

    [style] #20 - WorkoutViewController 탭바 추가

--- Routee-iOS/Routee-iOS/Presentation/TabBar/TabBarViewController.swift
+++ Routee-iOS/Routee-iOS/Presentation/TabBar/TabBarViewController.swift
@@ -62,7 +62,7 @@ final class TabBarViewController: UITabBarController {
     // MARK: - Configure
     
     private func configureViewControllers() {
-        let workout = UINavigationController(rootViewController: SampleViewController())
+        let workout = UINavigationController(rootViewController: WorkoutViewController())
         let recordEdit = UINavigationController(rootViewController: RecordEditViewController())
         let archive = UINavigationController(rootViewController: SampleViewController())
         let setting = UINavigationController(rootViewController: SampleViewController())
