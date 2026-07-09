87faadf57bd23f8b907b82454bb3b2e0f8673704
ser0kim <srkim0728@gmail.com>
2026-07-09 18:17:08 +0900

    [style] #21 - 상단 네비게이션바 구현

---
+++ Routee-iOS/Routee-iOS/Presentation/Common/Components/TopNavigationBar.swift
@@ -0,0 +1,86 @@
+//
+//  TopNavigationBar.swift
+//  Routee-iOS
+//
+//  Created by 김세령 on 7/9/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class TopNavigationBar: BaseUIView {
+    
+    // MARK: - Properties
+    
+    var backButtonAction: (() -> Void)?
+    var rightButtonAction: (() -> Void)?
+    private let rightTitle: String
+    
+    // MARK: - UI Properties
+    
+    private let backButton = UIButton()
+    private let rightButton = UIButton()
+    
+    // MARK: - Initializer
+    
+    init(rightTitle: String) {
+        self.rightTitle = rightTitle
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI Setting
+    
+    override func setStyle() {
+        backButton.do {
+            $0.setImage(UIImage(named: "ic_arrow_left_sm_white"), for: .normal)
+            $0.addTarget(self, action: #selector(didTapBackButton), for: .touchUpInside)
+        }
+        
+        rightButton.do {
+            $0.setTitle(rightTitle, for: .normal)
+            $0.setTitleColor(.staticWhite, for: .normal)
+            $0.titleLabel?.font = .label_sb_18
+            $0.addTarget(self, action: #selector(didTapRightButton), for: .touchUpInside)
+        }
+    }
+    
+    override func setUI() {
+        addSubviews(backButton, rightButton)
+    }
+    
+    override func setLayout() {
+        snp.makeConstraints {
+            $0.height.equalTo(44)
+        }
+        
+        backButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(4)
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(42)
+        }
+        
+        rightButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(4)
+            $0.centerY.equalToSuperview()
+            $0.height.equalTo(42)
+        }
+    }
+    
+    // MARK: - Actions
+
+    @objc
+    private func didTapBackButton() {
+        backButtonAction?()
+    }
+    
+    @objc
+    private func didTapRightButton() {
+        rightButtonAction?()
+    }
+}
