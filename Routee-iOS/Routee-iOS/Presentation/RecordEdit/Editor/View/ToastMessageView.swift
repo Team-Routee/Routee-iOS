ecf8225e6f853a4b508ca323841c9f1d48e52af7
ser0kim <srkim0728@gmail.com>
2026-07-11 21:25:15 +0900

    [feat] #33 - ToastMessageView 구현

---
+++ Routee-iOS/Routee-iOS/Presentation/RecordEdit/Editor/View/ToastMessageView.swift
@@ -0,0 +1,55 @@
+//
+//  ToastMessageView.swift
+//  Routee-iOS
+//
+//  Created by 김세령 on 7/11/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class ToastMessageView: BaseUIView {
+    
+    // MARK: - UI Properties
+    
+    let titleLabel = UILabel()
+    
+    // MARK: - init
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        titleLabel.text = title
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    // MARK: - UI Setting
+    
+    override func setStyle() {
+        backgroundColor = .grey_800
+        
+        titleLabel.do {
+            $0.textColor = .staticWhite
+            $0.font = .label_m_14
+            $0.textAlignment = .center
+            $0.numberOfLines = 1
+            $0.lineBreakMode = .byTruncatingTail
+        }
+    }   
+    
+    override func setUI() {
+        addSubview(titleLabel)
+    }
+    
+    override func setLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+}
