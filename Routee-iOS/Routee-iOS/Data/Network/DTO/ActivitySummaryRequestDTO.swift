a0fb46f023251e3f8220f55441b95915fc876026
seunghee <daalgwabyul@naver.com>
2026-07-14 23:54:24 +0900

    [network] #47 - ActivitySummary DTO 이름 변경

--- Routee-iOS/Routee-iOS/Data/Network/DTO/ActivitySummaryRequestDTO.swift
+++ Routee-iOS/Routee-iOS/Data/Network/DTO/ActivitySummaryRequestDTO.swift
@@ -1,12 +1,12 @@
 //
-//  ArchiveRequestDTO.swift
+//  ActivitySummaryRequestDTO.swift
 //  Routee-iOS
 //
 //  Created by 초긍정행운의포춘쿠키 on 7/14/26.
 //
 import Foundation
 
-struct ArchiveRequestDTO: Encodable {
+struct ActivitySummaryRequestDTO: Encodable {
     let year: Int
     let month: Int
 }
--- Routee-iOS/Routee-iOS/Data/Network/DTO/ActivitySummaryResponseDTO.swift
+++ Routee-iOS/Routee-iOS/Data/Network/DTO/ActivitySummaryResponseDTO.swift
@@ -1,5 +1,5 @@
 //
-//  ArchiveResponseDTO.swift
+//  ActivitySummaryResponseDTO.swift
 //  Routee-iOS
 //
 //  Created by 초긍정행운의포춘쿠키 on 7/14/26.
@@ -7,12 +7,12 @@
 
 import Foundation
 
-struct ArchiveResponseDTO: Decodable, Sendable {
-    let result: [ArchiveResult]
+struct ActivitySummaryResponseDTO: Decodable, Sendable {
+    let result: [ActivitySummaryResult]
     let year: Int
     let month: Int
 
-    struct ArchiveResult: Decodable, Sendable {
+    struct ActivitySummaryResult: Decodable, Sendable {
         let activityDate: String
         let totalDurationMinutes: Int
         let activityCount: Int
@@ -20,7 +20,7 @@ struct ArchiveResponseDTO: Decodable, Sendable {
     }
 }
 
-extension ArchiveResponseDTO {
+extension ActivitySummaryResponseDTO {
     func toModel() -> [ArchiveModel] {
         result.map {
             ArchiveModel(
