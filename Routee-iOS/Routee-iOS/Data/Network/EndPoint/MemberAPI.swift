4ae15715da3eddd381a1d215eb293acffb5f2e6b
seunghee <daalgwabyul@naver.com>
2026-07-15 00:16:48 +0900

    [network] #47 - Profile API 요청 함수 작성

--- Routee-iOS/Routee-iOS/Data/Network/EndPoint/MemberAPI.swift
+++ Routee-iOS/Routee-iOS/Data/Network/EndPoint/MemberAPI.swift
@@ -12,12 +12,13 @@ import Alamofire
 enum MemberAPI {
     case register(header: HeaderType, requestDTO: RegisterRequestDTO)
     case withdraw(header: HeaderType, requestDTO: WithdrawRequestDTO)
+    case getProfile(header: HeaderType)
 }
 
 extension MemberAPI: RouteeEndPoint {
     var basePath: String {
         switch self {
-        case .register, .withdraw:
+        case .register, .withdraw, .getProfile:
             return "/api/v1/member"
         }
     }
@@ -28,6 +29,8 @@ extension MemberAPI: RouteeEndPoint {
             return "/register"
         case .withdraw:
             return "/withdraw"
+        case .getProfile:
+            return "/profile"
         }
     }
     
@@ -37,12 +40,16 @@ extension MemberAPI: RouteeEndPoint {
             return  .post
         case .withdraw:
             return .delete
+        case .getProfile:
+            return .get
         }
     }
     
     var headers: HeaderType {
         switch self {
         case .register(let header, _), .withdraw(let header, _):
+            return header
+        case .getProfile(let header):
             return header
         }
     }
@@ -51,6 +58,8 @@ extension MemberAPI: RouteeEndPoint {
         switch self {
         case .register, .withdraw:
             return JSONEncoding.default
+        case .getProfile:
+            return URLEncoding.default
         }
     }
     
@@ -64,6 +73,8 @@ extension MemberAPI: RouteeEndPoint {
             return dto.asParameters()
         case .withdraw(_, let dto):
             return dto.asParameters()
+        case .getProfile:
+            return nil
         }
     }
 }
