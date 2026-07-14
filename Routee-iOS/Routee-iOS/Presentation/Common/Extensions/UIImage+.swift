d4bc13de818fbd69128ba986ac7fe4326e9adeaa
sangyup <sangyeobi712@gmail.com>
2026-07-15 04:12:56 +0900

    [feat] #52 - thumbnailImage extension 추가

--- Routee-iOS/Routee-iOS/Presentation/Common/Extensions/UIImage+.swift
+++ Routee-iOS/Routee-iOS/Presentation/Common/Extensions/UIImage+.swift
@@ -10,9 +10,33 @@ import UIKit
 extension UIImage {
     func resized(to size: CGSize) -> UIImage {
         let renderer = UIGraphicsImageRenderer(size: size)
-        
+
         return renderer.image { _ in
             draw(in: CGRect(origin: .zero, size: size))
+        }
+    }
+
+    func thumbnailImage(
+        borderWidth: CGFloat,
+        borderColor: UIColor,
+        cornerRadius: CGFloat
+    ) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: size)
+
+        return renderer.image { _ in
+            let rect = CGRect(origin: .zero, size: size)
+            let clipPath = UIBezierPath(roundedRect: rect, cornerRadius: cornerRadius)
+            clipPath.addClip()
+            draw(in: rect)
+
+            let borderRect = rect.insetBy(dx: borderWidth / 2, dy: borderWidth / 2)
+            let borderPath = UIBezierPath(
+                roundedRect: borderRect,
+                cornerRadius: max(cornerRadius - borderWidth / 2, 0)
+            )
+            borderPath.lineWidth = borderWidth
+            borderColor.setStroke()
+            borderPath.stroke()
         }
     }
 }
