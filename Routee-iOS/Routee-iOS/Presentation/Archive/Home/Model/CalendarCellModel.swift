47d236eccc77c463c6426c8ac1a3726fed01756a
seunghee <daalgwabyul@naver.com>
2026-07-15 13:31:21 +0900

    [chore] #47 - 캘린더 셀 모델 분리

---
+++ Routee-iOS/Routee-iOS/Presentation/Archive/Home/Model/CalendarCellModel.swift
@@ -0,0 +1,38 @@
+//
+//  CalendarCellModel.swift
+//  Routee-iOS
+//
+
+import Foundation
+
+struct CalendarCellModel {
+
+    enum Content {
+        case empty
+        case day(Int)
+    }
+
+    enum RecordState {
+        case none
+        case single
+        case multiple(Int)
+    }
+
+    let content: Content
+    let recordState: RecordState
+    let coverImageName: String?
+    let activityDate: String?
+}
+
+extension CalendarCellModel.RecordState {
+
+    init(activityCount: Int) {
+        if activityCount == 0 {
+            self = .none
+        } else if activityCount == 1 {
+            self = .single
+        } else {
+            self = .multiple(min(activityCount, 9))
+        }
+    }
+}
