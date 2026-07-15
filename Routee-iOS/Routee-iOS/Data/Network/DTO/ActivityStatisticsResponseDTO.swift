c80b8301b41bfa14f6d9fb948411cd06c1298ce0
seunghee <daalgwabyul@naver.com>
2026-07-15 16:51:43 +0900

    [network] #54 - ActivityStatisticsResponseDTO 작성

---
+++ Routee-iOS/Routee-iOS/Data/Network/DTO/ActivityStatisticsResponseDTO.swift
@@ -0,0 +1,28 @@
+//
+//  ActivityStatisticsResponseDTO.swift
+//  Routee-iOS
+//
+
+import Foundation
+
+struct ActivityStatisticsResponseDTO: Decodable, Sendable {
+    let activityId: Int64
+    let title: String
+    let activityDate: String
+    let distanceMeter: Int
+    let durationSec: Int
+    let maxElevationMeter: Int
+}
+
+extension ActivityStatisticsResponseDTO {
+    func toModel() -> ActivityStatisticsModel {
+        ActivityStatisticsModel(
+            activityId: activityId,
+            title: title,
+            activityDate: activityDate,
+            distanceMeter: distanceMeter,
+            durationSec: durationSec,
+            maxElevationMeter: maxElevationMeter
+        )
+    }
+}
