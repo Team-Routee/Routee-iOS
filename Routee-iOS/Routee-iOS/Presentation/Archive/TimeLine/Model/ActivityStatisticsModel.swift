64e2ce8a7795913214b44ed5f51fb9e90c18d7c4
seunghee <daalgwabyul@naver.com>
2026-07-15 16:52:06 +0900

    [network] #54 - ActivityStatisticsModel 작성

---
+++ Routee-iOS/Routee-iOS/Presentation/Archive/TimeLine/Model/ActivityStatisticsModel.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityStatisticsModel.swift
+//  Routee-iOS
+//
+//  Created by 초긍정행운의포춘쿠키 on 7/15/26.
+//
+
+import Foundation
+
+struct ActivityStatisticsModel {
+    let activityId: Int64
+    let title: String
+    let activityDate: String
+    let distanceMeter: Int
+    let durationSec: Int
+    let maxElevationMeter: Int
+}
