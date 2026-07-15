dd05264787af5380f1c2359f9d38c5cda4b94880
seunghee <daalgwabyul@naver.com>
2026-07-15 17:10:38 +0900

    [network] #54 - Track뷰에  trackPoint  데이터 연결

--- Routee-iOS/Routee-iOS/Presentation/Archive/TimeLine/ViewModel/TimeLineViewModel.swift
+++ Routee-iOS/Routee-iOS/Presentation/Archive/TimeLine/ViewModel/TimeLineViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  ActivityStatisticsViewModel.swift
+//  TimeLineViewModel.swift
 //  Routee-iOS
 //
 //  Created by 초긍정행운의포춘쿠키 on 7/15/26.
@@ -13,7 +13,7 @@ struct ActivityStatisticsMetricViewModel {
     let altitude: String
 }
 
-final class ActivityStatisticsViewModel {
+final class TimeLineViewModel {
 
     // MARK: - Properties
 
@@ -30,6 +30,11 @@ final class ActivityStatisticsViewModel {
     func fetchActivityStatistics(activityId: Int64) async throws -> ActivityStatisticsMetricViewModel {
         let model = try await activityRepository.getActivityStatistics(activityId: activityId)
         return makeMetricViewModel(from: model)
+    }
+
+    func fetchActivityRoute(activityId: Int64) async throws -> [TrackPoint] {
+        let model = try await activityRepository.getActivityRoute(activityId: activityId)
+        return model.trackPoints
     }
 
     // MARK: - Private Methods
