7ab3f0239ab45bc0de027bca1f8bbaf407b6dee4
ser0kim <srkim0728@gmail.com>
2026-07-15 13:18:46 +0900

    [network] #50 - 활동 경로 데이터 조회 API 레포지토리 구현

---
+++ Routee-iOS/Routee-iOS/Data/Network/Repository/ActivityRepository.swift
@@ -0,0 +1,42 @@
+//
+//  ActivityRepository.swift
+//  Routee-iOS
+//
+//  Created by 김세령 on 7/15/26.
+//
+
+protocol ActivityRepository {
+    func getActivityRoute(activityId: Int64) async throws -> ActivityRouteResponseDTO
+}
+
+struct DefaultActivityRepository: ActivityRepository {
+
+    private let service: NetworkService
+    private let keychainService: KeychainService
+
+    init(
+        service: NetworkService = DefaultNetworkService(),
+        keychainService: KeychainService = DefaultKeychainService()
+    ) {
+        self.service = service
+        self.keychainService = keychainService
+    }
+
+    func getActivityRoute(activityId: Int64) async throws -> ActivityRouteResponseDTO {
+        let accessToken = keychainService.read(.accessToken)
+
+        guard !accessToken.isEmpty else {
+            throw RouteeError.noData
+        }
+
+        let endPoint = ActivityAPI.activityRoute(
+            header: .withAuth(accessToken: accessToken),
+            activityId: activityId
+        )
+
+        return try await service.request(
+            endPoint,
+            decodingType: ActivityRouteResponseDTO.self
+        )
+    }
+}
