269dee6785c820b310e3305a6ddadecda9c3555a
seunghee <daalgwabyul@naver.com>
2026-07-15 22:35:51 +0900

    [network] #54 - TrackMap용 마커모델 추가

---
+++ Routee-iOS/Routee-iOS/Presentation/Archive/TimeLine/Model/ActivityRouteMarkerModel.swift
@@ -0,0 +1,16 @@
+//
+//  ActivityRouteMarkerModel.swift
+//  Routee-iOS
+//
+//  Created by 초긍정행운의포춘쿠키 on 7/15/26.
+//
+
+import Foundation
+
+struct ActivityRouteMarkerModel {
+    let timelineId: Int64
+    let thumbnailUrl: String
+    let latitude: Double
+    let longitude: Double
+    let pointIndex: Int
+}
