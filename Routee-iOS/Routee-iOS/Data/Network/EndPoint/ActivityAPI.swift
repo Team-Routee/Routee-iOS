813784df962069b8a66916d42f0a112d3ce55b61
sangyup <sangyeobi712@gmail.com>
2026-07-16 12:09:50 +0900

    [network] #58 - backgroundMap PresignedURL 네트워크 작성

--- Routee-iOS/Routee-iOS/Data/Network/EndPoint/ActivityAPI.swift
+++ Routee-iOS/Routee-iOS/Data/Network/EndPoint/ActivityAPI.swift
@@ -14,13 +14,18 @@ enum ActivityAPI {
     case createActivity(header: HeaderType, requestDTO: ActivityCreateRequestDTO)
     case timeLinePresignedURL(header: HeaderType, activityId: Int64, requestDTO: TimeLinePresignedURLRequestDTO)
     case createTimeLine(header: HeaderType, activityId: Int64, requestDTO: CreateTimeLineRequestDTO)
+    case backgroundMapPresignedURL(header: HeaderType, activityId: Int64, requestDTO: BackgroundMapPresignedURLRequestDTO)
 }
 
 extension ActivityAPI: RouteeEndPoint {
 
     var basePath: String {
         switch self {
-        case .activityRoute, .createActivity, .timeLinePresignedURL, .createTimeLine:
+        case .activityRoute,
+             .createActivity,
+             .timeLinePresignedURL,
+             .createTimeLine,
+             .backgroundMapPresignedURL:
             return "/api/v1/activity"
         }
     }
@@ -35,6 +40,8 @@ extension ActivityAPI: RouteeEndPoint {
             return "/\(activityId)/image-url"
         case .createTimeLine(_, let activityId, _):
             return "/\(activityId)/timeline"
+        case .backgroundMapPresignedURL(_, let activityId, _):
+            return "/\(activityId)/map-image-url"
         }
     }
 
@@ -42,15 +49,18 @@ extension ActivityAPI: RouteeEndPoint {
         switch self {
         case .activityRoute:
             return .get
-        case .createActivity, .timeLinePresignedURL, .createTimeLine:
+        case .createActivity, .timeLinePresignedURL, .createTimeLine, .backgroundMapPresignedURL:
             return .post
         }
     }
 
     var headers: HeaderType {
         switch self {
-        case .activityRoute(let header, _), .createActivity(let header, _),
-                .timeLinePresignedURL(let header, _, _), .createTimeLine(let header, _, _):
+        case .activityRoute(let header, _),
+             .createActivity(let header, _),
+             .timeLinePresignedURL(let header, _, _),
+             .createTimeLine(let header, _, _),
+             .backgroundMapPresignedURL(let header, _, _):
             return header
         }
     }
@@ -59,14 +69,14 @@ extension ActivityAPI: RouteeEndPoint {
         switch self {
         case .activityRoute:
             return URLEncoding.default
-        case .createActivity, .timeLinePresignedURL, .createTimeLine:
+        case .createActivity, .timeLinePresignedURL, .createTimeLine, .backgroundMapPresignedURL:
             return JSONEncoding.default
         }
     }
 
     var queryParameters: [String: String]? {
         switch self {
-        case .activityRoute, .createActivity, .timeLinePresignedURL, .createTimeLine:
+        case .activityRoute, .createActivity, .timeLinePresignedURL, .createTimeLine, .backgroundMapPresignedURL:
             return nil
         }
     }
@@ -80,6 +90,8 @@ extension ActivityAPI: RouteeEndPoint {
         case .timeLinePresignedURL(_, _, let requestDTO):
             return requestDTO.asParameters()
         case .createTimeLine(_, _, let requestDTO):
+            return requestDTO.asParameters()
+        case .backgroundMapPresignedURL(_, _, let requestDTO):
             return requestDTO.asParameters()
         }
     }
--- Routee-iOS/Routee-iOS/Data/Network/Repository/ActivityRepository.swift
+++ Routee-iOS/Routee-iOS/Data/Network/Repository/ActivityRepository.swift
@@ -13,13 +13,14 @@ protocol ActivityRepository {
     func timeLinePresignedURL(activityId: Int64, fileName: String) async throws -> ImagePresignedURLModel
     func uploadTimeLineImage(presignedURL: String, imageData: Data) async throws
     func createTimeLine(activityId: Int64, requestDTO: CreateTimeLineRequestDTO) async throws
+    func backgroundMapPresignedURL(activityId: Int64, requestDTO: BackgroundMapPresignedURLRequestDTO) async throws -> ImagePresignedURLModel
 }
 
 struct DefaultActivityRepository: ActivityRepository {
-
+    
     private let service: NetworkService
     private let keychainService: KeychainService
-
+    
     init(
         service: NetworkService = DefaultNetworkService(),
         keychainService: KeychainService = DefaultKeychainService()
@@ -27,48 +28,48 @@ struct DefaultActivityRepository: ActivityRepository {
         self.service = service
         self.keychainService = keychainService
     }
-
+    
     func getActivityRoute(activityId: Int64) async throws -> ActivityEditorModel {
         let accessToken = keychainService.read(.accessToken)
-
+        
         guard !accessToken.isEmpty else {
             throw RouteeError.noData
         }
-
+        
         let endPoint = ActivityAPI.activityRoute(
             header: .withAuth(accessToken: accessToken),
             activityId: activityId
         )
-
+        
         let response = try await service.request(
             endPoint,
             decodingType: ActivityRouteResponseDTO.self
         )
-
+        
         return response.toModel()
     }
     
     func createActivity(activityType: String, startedAt: String)
-        async throws -> WorkoutRecordStartModel {
-            let accessToken = keychainService.read(.accessToken)
-            
-            guard !accessToken.isEmpty else {
-                throw RouteeError.forbidden
-            }
-            
-            let dto = ActivityCreateRequestDTO(activityType: activityType, startedAt: startedAt)
-            
-            let endpoint = ActivityAPI.createActivity(
-                header: .withAuthTimeZone(
-                    accessToken: accessToken,
-                    timeZone: TimeZone.current.identifier
-                ),
-                requestDTO: dto
-            )
-            
-            let response = try await service.request(endpoint, decodingType: ActivityCreateResponseDTO.self)
-            return response.toWorkoutRecordStartModel()
+    async throws -> WorkoutRecordStartModel {
+        let accessToken = keychainService.read(.accessToken)
+        
+        guard !accessToken.isEmpty else {
+            throw RouteeError.forbidden
         }
+        
+        let dto = ActivityCreateRequestDTO(activityType: activityType, startedAt: startedAt)
+        
+        let endpoint = ActivityAPI.createActivity(
+            header: .withAuthTimeZone(
+                accessToken: accessToken,
+                timeZone: TimeZone.current.identifier
+            ),
+            requestDTO: dto
+        )
+        
+        let response = try await service.request(endpoint, decodingType: ActivityCreateResponseDTO.self)
+        return response.toWorkoutRecordStartModel()
+    }
     
     func timeLinePresignedURL(activityId: Int64, fileName: String) async throws -> ImagePresignedURLModel {
         let accessToken = keychainService.read(.accessToken)
@@ -76,38 +77,57 @@ struct DefaultActivityRepository: ActivityRepository {
         guard !accessToken.isEmpty else {
             throw RouteeError.forbidden
         }
-
+        
         let dto = TimeLinePresignedURLRequestDTO(fileName: fileName)
-
+        
         let endpoint = ActivityAPI.timeLinePresignedURL(
             header: .withAuth(accessToken: accessToken),
             activityId: activityId,
             requestDTO: dto
         )
-
+        
         let response = try await service.request(endpoint, decodingType: TimeLinePresignedURLResponseDTO.self)
         return response.toImagePresignedURLModel()
     }
-
+    
     func uploadTimeLineImage(presignedURL: String, imageData: Data) async throws {
         try await service.presignedURLUploadData(imageData, to: presignedURL, contentType: "image/jpeg")
     }
     
     func createTimeLine(activityId: Int64, requestDTO: CreateTimeLineRequestDTO) async throws {
         let accessToken = keychainService.read(.accessToken)
-
+        
         guard !accessToken.isEmpty else {
             throw RouteeError.forbidden
         }
-
+        
         let dto = requestDTO
-
+        
         let endpoint = ActivityAPI.createTimeLine(
             header: .withAuthTimeZone(accessToken: accessToken, timeZone: TimeZone.current.identifier),
             activityId: activityId,
             requestDTO: dto
         )
-
+        
         try await service.requestEmpty(endpoint)
+    }
+    
+    func backgroundMapPresignedURL(activityId: Int64, requestDTO: BackgroundMapPresignedURLRequestDTO) async throws -> ImagePresignedURLModel{
+        let accessToken = keychainService.read(.accessToken)
+        
+        guard !accessToken.isEmpty else {
+            throw RouteeError.forbidden
+        }
+        
+        let dto = requestDTO
+        
+        let endpoint = ActivityAPI.backgroundMapPresignedURL(
+            header: .withAuth(accessToken: accessToken),
+            activityId: activityId,
+            requestDTO: requestDTO
+        )
+        
+        let response = try await service.request(endpoint, decodingType: BackgroundMapPresignedURLResponseDTO.self)
+        return response.toImagePresignedURLModel()
     }
 }
