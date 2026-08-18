173297c3155d6665a092a8f8499e2e0953695b71
seunghee <daalgwabyul@naver.com>
2026-08-18 16:46:00 +0900

    [chore] #88 - 변경된 변수명 참조 맞춤

--- Routee-iOS/Routee-iOS/Presentation/Common/Components/NicknameTextField.swift
+++ Routee-iOS/Routee-iOS/Presentation/Common/Components/NicknameTextField.swift
@@ -10,7 +10,7 @@ import UIKit
 import SnapKit
 import Then
 
-final class NickNameTextField: UITextField {
+final class NicknameTextField: UITextField {
 
     enum NicknameTextFieldCase {
         case onboarding
