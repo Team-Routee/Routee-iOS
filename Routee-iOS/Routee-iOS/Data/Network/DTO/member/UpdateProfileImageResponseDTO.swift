06b880f426c00689a77872e02be9814413355d69
seunghee <daalgwabyul@naver.com>
2026-08-30 23:53:44 +0900

    [network] #111 - 프로필 사진 변경 API의 DTO 작성

---
+++ Routee-iOS/Routee-iOS/Data/Network/DTO/member/UpdateProfileImageResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  UpdateProfileImageResponseDTO.swift
+//  Routee-iOS
+//
+//  Created by 초긍정행운의포춘쿠키 on 8/30/26.
+//
+
+import Foundation
+
+struct UpdateProfileImageResponseDTO: Decodable, Sendable {
+    let profileImageUrl: String
+}
