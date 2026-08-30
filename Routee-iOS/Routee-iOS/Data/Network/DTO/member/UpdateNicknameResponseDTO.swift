f8bf60e6fde3d56250533f4bd0662c2a98530a07
seunghee <daalgwabyul@naver.com>
2026-08-30 23:52:50 +0900

    [network] #111 - 닉네임 변경 API의 DTO 작성

---
+++ Routee-iOS/Routee-iOS/Data/Network/DTO/member/UpdateNicknameResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  UpdateNicknameResponseDTO.swift
+//  Routee-iOS
+//
+//  Created by 초긍정행운의포춘쿠키 on 8/30/26.
+//
+
+import Foundation
+
+struct UpdateNicknameResponseDTO: Decodable, Sendable {
+    let nickname: String
+}
